0ae3d842dc5e91c26e5803f0912f0a6d281e2114
Navyacs-crypto <navyacshaji12@gmail.com>
2025-03-27 13:04:55 +0530

    Add commented-out code snippets for largest even number, repeated number count, and frequency number count

--- index.js
+++ index.js
@@ -20,7 +20,54 @@
 
 
 
+//Largest Even Number...............................................
+
+// let arr=[1,5,7,3,8,67,34]
+// let Largesteven=arr[0]
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]%2==0>Largesteven){
+//        Largesteven=arr[i]
+//     }
+// }
+// console.log(Largesteven)
 
 
+//Repeated Number Count..........................................
+
+// let arr=[1,2,1,3,4,3,5,5,6];
+// let count=0;
+// for(let i=0;i<arr.length-1;i++){
+//     let temp=0
+//     for(let j=i+1;j<arr.length;j++){
+//         if(arr[i]==arr[j]){
+//             let temp=1
+//             count++
+//         }
+//     }
+// }
+// console.log(count)
+
+
+//Frequency Number Count..........................................
+
+// let arr=[1,4,2,3,5,7,1,1,2,2,8,9,10]
+// let freq={}
+// for(let i=0;i<arr.length;i++){
+//     let count=0
+
+//     for(let j=0;j<arr.length;j++){
+//         if(arr[i]==arr[j]){
+//             count++
+//         }
+       
+//         }
+//          if(!freq[arr[i]]){
+//             freq[arr[i]]=count
+//     }
+    
+// }
+// for(let key in freq){
+//     console.log(key,freq[key])
+// }
 
 
