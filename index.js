ea08a926d5cd9e6102e7a245c8184eda69bc0ba2
Navyacs-crypto <navyacshaji12@gmail.com>
2025-03-27 10:19:28 +0530

    QA First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// Create a new array containing all the numbers between the minimum and maximum values.
+
+// let a=[1,8,7,9,10]
+// let size=0;
+// let empty=[]
+// for(let i=0;i<a.length-1;i++){
+//     for(let j=i+1;j<a.length;j++){
+//         if(a[i]>a[j]){
+//             [a[i],a[j]]=[a[j],a[i]]
+//         }
+//     }
+// }
+// let Max=a[a.length-1]
+// let Min=a[0]
+// for(let k=Min;k<=Max;k++){
+//     empty[size]=k
+//     size++
+// }
+// console.log(empty)
+
+
+
+
+
+
+
